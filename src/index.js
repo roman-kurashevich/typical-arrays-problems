27b3205bd6e4128672764fa6cb8d192e789c1213
Roman Kurashevich <rkurashevich@gmail.com>
2021-02-20 23:19:48 +0700

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let minElem = array[0];
+    for(let i of array) {
+      if (i < minElem) {minElem = i}
+    }
+    return minElem;
+  } else return 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    let maxElem = array[0];
+    for(let i of array) {
+      if (i > maxElem) {maxElem = i}
+    }
+    return maxElem;
+  } else return 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (Array.isArray(array) && array.length) {
+    return ((array.reduce((sum,current) => sum + current, 0))/(array.length))
+  } else return 0;
 }
